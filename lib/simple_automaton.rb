28841855cf0a5eb93a9165183bc58e53b5b21134
Stuart Gale <bishboria@gmail.com>
2012-05-07 22:57:56 +0100

    Separating production and test code.

---
+++ lib/simple_automaton.rb
@@ -0,0 +1,27 @@
+class State
+  def input message
+    fail "Override me"
+  end
+
+  def == other
+    self.class == other.class
+  end
+end
+
+class StateA < State
+  def input message
+    [self, StateB.new][message.to_i]
+  end
+end
+
+class StateB < State
+  def input message
+    [StateA.new, StateC.new][message.to_i]
+  end
+end
+
+class StateC < State
+  def input message
+    StateC.new
+  end
+end
--- spec/simple_automaton_spec.rb
+++ spec/simple_automaton_spec.rb
@@ -1,33 +1,5 @@
 require "rspec"
 
-class State
-  def input message
-    fail "Override me"
-  end
-
-  def == other
-    self.class == other.class
-  end
-end
-
-class StateA < State
-  def input message
-    [self, StateB.new][message.to_i]
-  end
-end
-
-class StateB < State
-  def input message
-    [StateA.new, StateC.new][message.to_i]
-  end
-end
-
-class StateC < State
-  def input message
-    StateC.new
-  end
-end
-
 describe "simple automaton" do
   def state_after_message message
     starting_state.input message
