fb99d81757d0a4b5df27fda26c0554d38978ea73
Stuart Gale <bishboria@gmail.com>
2012-05-07 21:11:01 +0100

    .

--- simple_automaton.rb
+++ simple_automaton.rb
@@ -14,7 +14,7 @@ describe "simple automaton" do
     it "transistions to A when it receives 0" do
       state = State.new :A
       new_state = state.input "0"
-      new_state.should == "0"
+      new_state.state.should == "0"
     end
   end
 end
