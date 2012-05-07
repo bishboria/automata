7aebf8a8ad9dd08b90f7bde457c5bf97d3501c64
Stuart Gale <bishboria@gmail.com>
2012-05-07 22:14:56 +0100

    .

--- simple_automaton.rb
+++ simple_automaton.rb
@@ -61,7 +61,7 @@ describe "simple automaton" do
     let(:state) { StateC.new }
 
     it "transitions to C when it receives 0" do
-      state_after "0", StateA.new
+      state_after "0", StateC.new
     end
 
     it "transitions to C when it receives 1" do
