01e15e4535525bc3e35f9ac9a5077fbc8635926e
Stuart Gale <bishboria@gmail.com>
2012-05-07 21:21:24 +0100

    Oops... Testing wrong thing...

--- simple_automaton.rb
+++ simple_automaton.rb
@@ -22,7 +22,7 @@ describe "simple automaton" do
     it "transistions to A when it receives 0" do
       state = State.new :A
       new_state = state.input "0"
-      new_state.state.should == "0"
+      new_state.state.should == :B
     end
     
     it "transitions to B when it receives 1" do
