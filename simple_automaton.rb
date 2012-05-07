36c63cb425dcf223e5060022fd7806d73732335a
Stuart Gale <bishboria@gmail.com>
2012-05-07 21:45:21 +0100

    DECISION POINT! The table works fine.

--- simple_automaton.rb
+++ simple_automaton.rb
@@ -8,15 +8,15 @@ class State
   end
 
   def input message
-    #@table_rule = {
-      #A: [ (State.new :A), (State.new :B)],
-      #B: [ (State.new :A), (State.new :C)],
-      #C: [ (State.new :C), (State.new :C)]
-    #}
-    #@table_rule[@state][message.to_i]
-    return State.new :A if message == "0"
-    return State.new :C if @state == :B && message == "1"
-    State.new :B
+    @table_rule = {
+      A: [ (State.new :A), (State.new :B)],
+      B: [ (State.new :A), (State.new :C)],
+      C: [ (State.new :C), (State.new :C)]
+    }
+    @table_rule[@state][message.to_i]
+    #return State.new :A if message == "0"
+    #return State.new :C if @state == :B && message == "1"
+    #State.new :B
   end
 end
 
